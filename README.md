c1ee96e14b7a2193ba639c07e8d56589551b4f59
Dan Constantinescu <dan_constantinescu@outlook.com>
2018-02-13 13:44:58 +0200

    update

--- README.md
+++ README.md
@@ -4,14 +4,9 @@ It uses:
 #### React
 #### Redux
 #### Sass
-#### RxJS
 #### React-Router 4
-#### React-Redux
-#### Redux-Observable
-#### Axios
 #### HMR
 #### Supports Typescript
-#### Webpack Dashboard
 
 ```
 > git clone http://github.com/aircraft721/react-boilerplate.git
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
-const DashboardPlugin = require('webpack-dashboard/plugin');
 
 module.exports = merge(common, {
     module: {
@@ -18,6 +17,5 @@ module.exports = merge(common, {
     plugins: [
         new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new DashboardPlugin()
     ]
 })
